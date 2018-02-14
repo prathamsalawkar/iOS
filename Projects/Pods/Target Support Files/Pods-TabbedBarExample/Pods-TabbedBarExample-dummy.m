75f06ef18f552a7128b3d5bff7be528009a74215
Prathamesh Salawkar <pratham.salawkar@gmail.com>
2018-02-15 04:25:15 +0530

    Added pod project

---
+++ Projects/Pods/Target Support Files/Pods-TabbedBarExample/Pods-TabbedBarExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TabbedBarExample : NSObject
+@end
+@implementation PodsDummy_Pods_TabbedBarExample
+@end
---
+++ Projects/Pods/Target Support Files/Pods-TabbedBarExampleUITests/Pods-TabbedBarExampleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TabbedBarExampleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TabbedBarExampleUITests
+@end
